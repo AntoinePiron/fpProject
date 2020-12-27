064073cb3114155811909c58aeadbd090ee297d7
Antoine Piron <pro.piron.antoine@gmail.com>
2020-12-27 22:08:20 +0100

    added few proprety to our fullpage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 new fullpage('#fullPage', {
     autoScrolling: true,
     navigation: true,
+    anchors: ['section1', 'section2', 'section3'],
+    navigationTooltips: ['Section 1', 'Section 2', 'Section 3'],
+    scrollingSpeed: 750,
 });
