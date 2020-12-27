e2205e8b19f934dff16d401bf4b274625a899767
Antoine Piron <pro.piron.antoine@gmail.com>
2020-12-27 22:13:10 +0100

    added slides and slide navigation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ new fullpage('#fullPage', {
     anchors: ['section1', 'section2', 'section3'],
     navigationTooltips: ['Section 1', 'Section 2', 'Section 3'],
     scrollingSpeed: 750,
+    controlArrows: false,
+    slidesNavigation: true,
 });
